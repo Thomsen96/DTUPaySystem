6d98e944544119ca95932109c5bd46bbd4869fc0
Thomsen96 <jonastoftethomsen@gmail.com>
2022-01-20 13:04:11 +0100

    Fixed main branch

--- initRepo.sh
+++ initRepo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git pull
 git submodule update --init
-git submodule foreach git checkout main
 git submodule foreach git submodule update --init
+git submodule foreach git checkout main
 git submodule foreach git submodule foreach git checkout main
